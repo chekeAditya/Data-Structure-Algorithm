b605a8ae93a5f435414b0c9c664777ed33169c5f
AdityaCheke291 <adityacheke4@gmail.com>
2021-08-01 10:03:58 +0530

    BalancedBrackets

--- StackDS/BalancedBrackets.java
+++ StackDS/BalancedBrackets.java
@@ -1,7 +1,65 @@
+/**
+ * Given an expression string exp, write a program to examine whether the pairs and the orders of “{“, “}”, “(“, “)”, “[“, “]” are correct in exp.
+ * <p>
+ * Example:
+ * <p>
+ * Input: exp = “[()]{}{[()()]()}”
+ * Output: Balanced
+ * <p>
+ * Input: exp = “[(])”
+ * Output: Not Balanced
+ */
+
 package Basic_To_Advanced_DataStructure.StackDS;
+import java.util.Scanner;
+import java.util.Stack;
 
 public class BalancedBrackets {
-    public static void main(String[] args) {
+    static boolean res(String str) {
+        Stack<Character> stack = new Stack<>();
 
+        for (int i = 0; i < str.length(); i++) {
+            char x = str.charAt(i);
+
+            if (x == '(' || x == '[' || x == '{') {
+                stack.push(x);
+                continue;
+            }
+            if (stack.isEmpty())
+                return false;
+            char check;
+            switch (x) {
+                case ')':
+                    check = stack.pop();
+                    if (check == '{' || check == '[')
+                        return false;
+                    break;
+
+                case '}':
+                    check = stack.pop();
+                    if (check == '(' || check == '[')
+                        return false;
+                    break;
+
+                case ']':
+                    check = stack.pop();
+                    if (check == '(' || check == '{')
+                        return false;
+                    break;
+            }
+        }
+        return (stack.isEmpty());
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        while (n-- > 0) {
+            String str = scanner.next();
+            if (res(str))
+                System.out.println("balanced");
+            else
+                System.out.println("not balanced");
+        }
     }
 }
