77b6fecac0df9d15ada60dbc32bed59b78c10ed5
AdityaCheke291 <adityacheke4@gmail.com>
2021-08-07 11:34:58 +0530

    Implement GCD

--- Recursion/C_ImplementGCD.java
+++ Recursion/C_ImplementGCD.java
@@ -1,31 +1,5 @@
 /**
- * Implement GCD
- * Description
  *
- * Implement the GCD Function using recursion. Given two integers a and b, find the greatest common divisor of a and b.
- *
- * The approach which uses logarithmic time complexity is expected here.
- *
- *
- * Input
- * The first line of the input contains one integer t (1 ≤ t ≤ 10) — the number of test cases. Then t test cases follow.
- *
- * The first line of each test case contains two integers a and b (1 ≤ a, b ≤ 1000000000).
- *
- *
- * Output
- * For each test case, print the answer: GCD of the two numbers.
- *
- *
- * Sample Input 1
- *
- * 2
- * 6 9
- * 2 25
- * Sample Output 1
- *
- * 3
- * 1
  */
 package Basic_To_Advanced_DataStructure.Recursion;
 
@@ -39,9 +13,11 @@ public class C_ImplementGCD {
             int n = scanner.nextInt();
             int m = scanner.nextInt();
             System.out.println(res(n, m));
+            System.out.println(res1(n, m));
         }
     }
 
+    //First method where it is cheking all the conditions
     static int res(int n, int m) {
         if (n == 0) {
             return m;
@@ -55,5 +31,13 @@ public class C_ImplementGCD {
         if (n > m) {
             return res(n - m, m);
         } else return res(n, m - n);
+    }
+
+    //method two
+    static int res1(int n, int m) {
+        if (m == 0) {
+            return n;
+        } else
+            return res(m, n % m);
     }
 }
---
+++ Recursion/C_RecursiveBubbleSort.java
@@ -0,0 +1,7 @@
+package Basic_To_Advanced_DataStructure.Recursion;
+
+public class C_RecursiveBubbleSort {
+    public static void main(String[] args) {
+
+    }
+}
