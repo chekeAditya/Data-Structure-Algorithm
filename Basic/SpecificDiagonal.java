1a763d0327ca30b3f77fa389f1e43e734801b44b
Aditya Cheke <adityacheke4@gmail.com>
2021-12-20 20:57:34 +0530

    Functions

---
+++ Basic/SpecificDiagonal.java
@@ -0,0 +1,14 @@
+package Basic_To_Advanced_DataStructure.Basic;//package A_Contest_1085;
+//
+//import java.util.Scanner;
+//
+//public class SpecificDiagonal {
+//    public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+//        int n = scanner.nextInt();
+//        int m = scanner.nextInt();
+//        int[][] arr = new int[n][m];
+//        for (int i=0;i<n;i++)
+//            for (int j=0;)
+//    }
+//}
