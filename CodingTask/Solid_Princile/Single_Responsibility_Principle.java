71495ab6d3ab8d9790d60a15791b9fccf620fab3
Aditya Cheke <adityacheke4@gmail.com>
2022-01-03 19:36:50 +0530

    Single_Responsibility_Principle

---
+++ CodingTask/Solid_Princile/Single_Responsibility_Principle.java
@@ -0,0 +1,44 @@
+package Basic_To_Advanced_DataStructure.CodingTask.Solid_Princile;
+
+public class Single_Responsibility_Principle {
+
+}
+
+class User {
+
+    private String firstName;
+    private String lastName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
+
+class UserType extends User {
+
+    private int selecting;
+
+    public int getSelecting() {
+        return selecting;
+    }
+
+    public void setSelecting(int selecting) {
+        this.selecting = selecting;
+    }
+
+    public boolean isSpecialUser() {
+        return selecting == 7;
+    }
+}
