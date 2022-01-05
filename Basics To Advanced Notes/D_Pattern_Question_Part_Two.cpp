c17670670531966625abdc5104a6d4fd9742d878
Aditya Cheke <adityacheke4@gmail.com>
2022-01-05 22:02:35 +0530

    DSA

---
+++ Basics To Advanced Notes/D_Pattern_Question_Part_Two.cpp
@@ -0,0 +1,146 @@
+#include <iostream>
+
+int main() {
+
+    // Pattern Problems
+     * Print the below Pattern*
+1 2 3
+1 2 3
+1 2 3
+    int n,i = 1;
+    std::cout << "Enter the Number \n";
+    std::cin >> n;
+
+    while(i<=n){
+
+      int j=1;
+      while(j<=n){
+        std::cout << j << " ";
+        j++;
+      }
+      std::cout << "\n";
+      i++;
+    }
+
+
+  * Print the below Pattern*
+3 2 1
+3 2 1
+3 2 1
+    int n,i = 1;
+    std::cout << "Enter the Number \n";
+    std::cin >> n;
+
+    while(i<=n){
+
+      int j=1;
+      while(j<=n){
+        std::cout << n-j+1 << " ";
+        j++;
+      }
+      std::cout << "\n";
+      i++;
+    }
+
+
+  `` Print the below Pattern ``
+1 2 3
+4 5 6
+7 8 9
+int n,i=1,count = 1;
+std::cout << "Enter the Number \n";
+std::cin >> n;
+while(i<=n){
+
+  int j = 1;
+  while(j<=n){
+    std::cout << count << " ";
+    j++;
+    count++;
+  }
+  std::cout << "\n";
+  i++;
+}
+
+  `` Print the below Pattern ``
+*
+* *
+* * *
+* * * *
+  int n,i =1;
+  std::cout << "Enter the Number \n";
+  std::cin >> n;
+  while(i<=n){
+   int j = 1;
+    while(j<=i){
+      std::cout << "*" << " ";
+      j++;
+    }
+    std::cout << "\n";
+    i++;
+  }
+
+
+    ``Print the Below pattern``
+1
+2 2
+3 3 3
+4 4 4 4
+  int n,i =1;
+  std:: cout << "Enter the Number \n";
+  std:: cin >> n;
+
+  while(i<=n){
+    int j=1;
+    while(j<=i){
+      std::cout << i << " ";
+      j++;
+    }
+    std::cout<<"\n";
+    i++;
+  }
+
+
+    ``Print the Below pattern``
+1
+2 3
+4 5 6
+7 8 9 10
+  int n,i = 1,count = 1;
+  std:: cout << "Enter the Number \n";
+  std:: cin >> n;
+
+  while(i<=n){
+    int j=1;
+    while(j<=i){
+      std::cout << count << " ";
+      j++;
+      count++;
+    }
+    std::cout<<"\n";
+    i++;
+  }
+
+
+    ``Print the Below pattern``
+1
+2 3
+3 4 5
+4 5 6 7
+  int n, i=1;
+  std::cout << "Enter a Number \n";
+  std::cin >> n;
+
+  while(i<=n){
+
+      int j=1;
+      while(j<=i){
+       // std::cout << count << " "; //this is one way
+        std::cout << j+i-1 << " "; //this is another way
+        j++;
+      }
+      std::cout << "\n";
+      i++;
+  }
+
+}
