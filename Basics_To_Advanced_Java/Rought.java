66cef5e788b18288587a8aa4ed8994d51cd45e47
Aditya Cheke <adityacheke4@gmail.com>
2022-01-18 23:33:00 +0530

    Binary Search

---
+++ Basics_To_Advanced_Java/Rought.java
@@ -0,0 +1,23 @@
+package Basic_To_Advanced_DataStructure.Basics_To_Advanced_Java;
+
+
+import java.util.*;
+
+public class Rought {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 4;
+        a = a + b;
+        b = a - b;
+        System.out.println(a-b);
+        System.out.println(b);
+    }
+}
+/*
+"abcd", "abcde"
+"abcd", "abce"
+
+"abd", "abce"
+"abd", "abcde"
+1       1
+ */
